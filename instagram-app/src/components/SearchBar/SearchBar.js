fe134d68dde5b5a3cc8efabd2ab7792f288c6aee
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-03 12:49:18 +0100

    Create CSS for logo component

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ import './SearchBar.css';
 export default class SearchBar extends React.Component {
     render() {
         return (
-            <div>
+            <div className='SearchBar'>
                 <Logo />
             </div>
         );
