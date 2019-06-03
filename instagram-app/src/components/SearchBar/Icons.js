05adf1bd1fcdef6ab05de0b27699d211c0a96eb9
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-03 13:07:38 +0100

    Add styling to icons component, in turn finish SearchBar

--- instagram-app/src/components/SearchBar/Icons.js
+++ instagram-app/src/components/SearchBar/Icons.js
@@ -5,7 +5,7 @@ export default class Icons extends React.Component {
 
     render() {
         return (
-            <div className='Search'>
+            <div className='Icons'>
                 <i class="large material-icons">explore</i>
                 <i class="large material-icons">favorite_border</i>
                 <i class="large material-icons">person_outline</i>
