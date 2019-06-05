27bf3779e1698d186947417603857d7477fbf074
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-05 12:46:14 +0100

    Build out HOC withAuthenticate to accept PostsPage

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 
 import PostsPage from "./components/PostContainer/PostsPage";
+import withAuthenticate from "./authentication/withAuthenticate";
 import { dataWithIds } from "./dummy-data";
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
 
 class App extends React.Component {
   render() {
-      return (
-        <PostsPage dataWithIds={dataWithIds} />
-      )
-    }
+    return <ComponentFromWithAuthenticate dataWithIds={dataWithIds} />;
+  }
 }
 
 export default App;
