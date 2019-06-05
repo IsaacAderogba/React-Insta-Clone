0932b7ceff969b41c69f7b73db4c5b020f67273e
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-05 13:26:35 +0100

    Add state to Login.js

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,26 +1,51 @@
 import React from "react";
 import Instagram from "./instagram.png";
 
-
 import "./Login.css";
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  usernameInputHandler = input => {
+    this.setState({
+      username: input.target.value
+    });
+  };
+
+  passwordInputHandler = input => {
+    this.setState({
+      password: input.target.value
+    });
+  };
+
   render() {
     return (
       <div className="Login">
         <div className="InstagramPhoto">
-            <img src={Instagram} alt="Instagram"/>
+          <img src={Instagram} alt="Instagram" />
         </div>
         <form className="LoginForm">
-          <input
-            placeholder="Your name"
-            required
+          <input 
+          placeholder="Username" 
+          required 
+          onChange={this.usernameInputHandler}
+          value={this.state.username}
           />
-          <input
-            placeholder="Add a comment"
-            required
+          <input 
+          type="password"
+          placeholder="Password" 
+          required 
+          onChange={this.passwordInputHandler}
+          value={this.state.password}
           />
-          <button>Submit Comment</button>
+          <button>Login</button>
         </form>
       </div>
     );
